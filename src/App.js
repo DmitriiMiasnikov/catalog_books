ae95d894eb4f36082eabd14b7c38d0f2b583f31e
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-01-23 04:14:22 +0300

    fix version sass

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
+import styles from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.wrapper}>
+      app
     </div>
   );
 }
