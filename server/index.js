b8ac297fcb9648e0100cf128c0b64898629998de
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-01-26 00:48:15 +0300

    request done

--- server/index.js
+++ server/index.js
@@ -1,31 +1,48 @@
 const iconv = require("iconv-lite");
+var needle = require('needle');
 const fs = require("fs");
 const xpath = require("xpath");
 const dom = require("xmldom").DOMParser;
 const URL = "http://www.world-art.ru/animation/animation.php?id=1";
-const xmlString = `
-<HTML>
-<head>
-  <title>Official game sheet</title>
-  <custom>Here we are</custom>
-<body class="sheet">
-</BODY>
-</HTML>`;
-
-
-const { spawn } = require("child_process");
-const curl = spawn("curl", [URL]);
-let text = "";
-curl.stdout.on("data", (data) => {
-  text = iconv.decode(data, "win1251");
+// const xmlString = `
+// <HTML>
+// <head>
+//   <title>Official game sheet</title>
+//   <custom>Here we are</custom>
+// <body class="sheet">
+// </BODY>
+// </HTML>`;
+needle.get(URL, function(err, res){
+  if (err) throw err;
   path = "//title//text()";
-  const doc = new dom().parseFromString(xmlString);
+  const doc = new dom().parseFromString(res.body);
   const tmp = xpath.select(path, doc);
-  fs.writeFile('json.json', `{ 
-    "name": "${tmp.toString()}"
+  fs.writeFile('json.json', `{
+    "name": "${tmp}"
   }`, (err) => {
     if (err) throw err;
   })
 });
+
+// const { spawn } = require("child_process");
+// const curl = spawn("curl", [URL]);
+// let text = "";
+// curl.stdout.on("data", (data) => {
+//   text += data;
+// });
+// curl.stderr.on("data", (data) => {
+
+// });
+// curl.on('close', (code) => {
+//   text = iconv.decode(text, "utf8");
+//   path = "//title";
+//   const doc = new dom().parseFromString(text);
+//   const tmp = xpath.select(path, doc);
+//   fs.writeFile('json.json', `{
+//     "name": "${tmp}"
+//   }`, (err) => {
+//     if (err) throw err;
+//   })
+// });
 
 // /html/body/center/table[7]/tbody/tr/td/table/tbody/tr/td[5]/table[2]/tbody/tr[2]/td[3]/table[2]/tbody/tr/td[3]
