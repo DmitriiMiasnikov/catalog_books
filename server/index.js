9c7fb4e28c9d86bbfe71ed5e3cdee26418157061
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-01-26 14:55:37 +0300

    fix parse date and author

--- server/index.js
+++ server/index.js
@@ -12,9 +12,9 @@ for (let i = 1; i <= counter; i++) {
     pathNameRu = ".//*[table/tr/td/b[contains(text(),'Название (ромадзи)')]]/table[1]//text()";
     pathNameEng = ".//*[td/b[contains(text(),'Название (англ.)')]]/td[3]//text()";
     pathNameRom = ".//*[td/b[contains(text(),'Название (ромадзи)')]]/td[3]//text()";
-    pathAuthor = ".//*[td/b[contains(text(),'Режиссёр')]]/td[3]//text()";
-    pathDate = ".//*[td/b[contains(text(),'Премьера')]]/td[3]//text()";
-    pathGenre = ".//*[td/b[contains(text(),'Жанр')]]/td[3]//text()";
+    pathAuthor = ".//*[td/b[contains(text(),'Режиссёр')]]/td[3]/a//text()";
+    pathDate = ".//*[td/b[contains(text(),'Премьера')]]/td[3]/a//text()";
+    pathGenre = ".//*[td/b[contains(text(),'Жанр')]]/td[3]/a//text()";
     pathType = ".//*[td/b[contains(text(),'Тип')]]/td[3]//text()";
     pathAuditory = ".//*[td/b[contains(text(),'Целевая аудитория')]]/td[3]//text()";
     pathSubscription = ".//*[table//font[contains(text(),'Краткое содержание')]]/table[6]//text()";
@@ -23,14 +23,15 @@ for (let i = 1; i <= counter; i++) {
     const tmpNameRom = xpath.select(pathNameRom, doc);
     const tmpAuthor = xpath.select(pathAuthor, doc);
     let tmpDate = xpath.select(pathDate, doc);
-    if (!tmpDate) {
-      pathDate = ".//*[td/b[contains(text(),'Выпуск')]]/td[3]//text()";
-      tmpDate = xpath.select(pathDate, doc);
-    }
     const tmpGenre = xpath.select(pathGenre, doc);
     const tmpType = xpath.select(pathType, doc);
     const tmpAuditory = xpath.select(pathAuditory, doc);
     const tmpSubscription = xpath.select(pathSubscription, doc);
+    if (!tmpDate.length) {
+      pathDate1 = ".//*[td/b[contains(text(),'Выпуск')]]/td[3]/a[position() <= 3]//text()";
+      pathDate2 = ".//*[td/b[contains(text(),'Выпуск')]]/td[3]/a[position() > 3]//text()";
+      tmpDate = `${xpath.select(pathDate1, doc)} - ${xpath.select(pathDate1, doc)}`;
+    }
     fs.appendFileSync('data.json',
       `{
             "animeId": "${i}",
