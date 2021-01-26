ab65ba722f45de93c5ea7ecca4374802239b187b
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-01-26 23:26:26 +0300

    fix async request img

--- parsers/imgParse.js
+++ parsers/imgParse.js
@@ -3,23 +3,41 @@ const needle = require('needle');
 const xpath = require("xpath");
 const dom = require("xmldom").DOMParser;
 
-const counter = 5;
+const counter = 20;
+const arr = [];
 for (let i = 1; i <= counter; i++) {
-  const URL = `http://www.world-art.ru/animation/animation_poster.php?id=${i}&number_img=1`;
+  arr.push(i);
+}
+function delay() {
+  return new Promise(resolve => setTimeout(resolve, 500));
+}
+async function delayedLog(item) {
+  await delay();
+  const URL = `http://www.world-art.ru/animation/animation_poster.php?id=${item}&number_img=1`;
   needle.get(URL, function (err, res) {
     if (err) throw err;
     const doc = new dom({
       locator: {},
-      errorHandler: { warning: function (w) { }, 
-      error: function (e) { }, 
-      fatalError: function (e) { console.error(e) } }
-  }).parseFromString(res.body);
+      errorHandler: {
+        warning: function (w) { },
+        error: function (e) { },
+        fatalError: function (e) { console.error(e) }
+      }
+    }).parseFromString(res.body);
     pathImg = ".//*[table//a[contains(text(),'все')]]/table[1]//img/@src";
-    const tmpImg = xpath.select(pathImg, doc);
-    const curl = spawn('curl', ['-o', `./img/anime_cover_${i}.jpg`, `http://www.world-art.ru/animation/${tmpImg[0].value}`]);
-    curl.stdout.on('data', (data) => {
-      console.log(`stdout: ${data}`);
-    });
+    console.log(item, doc.toString().length);
+    if (pathImg) {
+      const tmpImg = xpath.select(pathImg, doc);
+      const curl = spawn('curl', ['-o', `./img/anime_cover_${item}.jpg`, `http://www.world-art.ru/animation/${tmpImg[0].value}`]);
+      curl.stdout.on('data', (data) => {
+        console.log(`stdout: ${data}`);
+      });
+    }
   })
-
-};
+}
+processArray(arr);
+async function processArray(arr) {
+  for (const item of arr) {
+    await delayedLog(item);
+  }
+}
