5ed1b6e5e0582f782848e8de73f6112986cc9a21
DmitriiMiasnikov <51396910+DmitriiMiasnikov@users.noreply.github.com>
2021-02-04 22:25:29 +0300

    fix registration page

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -4,13 +4,29 @@ import { RegistrationDom } from './RegistrationDom';
 import { userRegistration } from './../../store/usersReducer';
 
 const Registration = ({ userRegistration }) => {
-
-  const registrationHandler = (event) => {
-    event.preventDefault();
-    userRegistration('asda', 'asdasd', 'asdasd')
+  const inputs = [
+    {name: 'name', text: 'имя'},
+    {name: 'password', text: 'пароль'},
+    {name: 'repeatPassword', text: ' повторите пароль'},
+    {name: 'email', text: 'email'}
+  ]
+  const validate = (data) => {
+    const err = {};
+    if (!data.name) err.name = 'Введите имя'
+    if (!data.email) err.email = 'Введите почту'
+    if (!data.password) err.password = 'Введите пароль'
+    if (data.password && data.password.length > 10) err.password = 'Макс пароль 10 знаков'
+    if (!data.repeatPassword) {
+      err.repeatPassword = 'Повторите пароль'
+    } else if (data.password !== data.repeatPassword) err.repeatPassword = 'Не совпадает пароль'
+    // changeTextInput(data)
+    return err
+  }
+  const registrationHandler = (data) => {
+    userRegistration(data.name, data.password, data.email)
   }
   return (
-    <RegistrationDom registrationHandler={registrationHandler}/>
+    <RegistrationDom registrationHandler={registrationHandler} inputs={inputs} validate={validate}/>
   )
 }
 
--- src/components/Registration/RegistrationDom.js
+++ src/components/Registration/RegistrationDom.js
@@ -1,20 +1,39 @@
 import React from 'react';
+import { Field, Form } from 'react-final-form';
 import styles from './Registration.module.scss';
+import classnames from 'classnames';
 
-export const RegistrationDom = ({ registrationHandler }) => {
+export const RegistrationDom = ({ registrationHandler, inputs, validate }) => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.registrationBlock}>
-        <form>
-          <input placeholder={'имя'} />
-          <input placeholder={'пароль'} />
-          <input placeholder={'email'} />
-          <div className={styles.buttons}>
-              <div className={styles.button} onClick={(e) => registrationHandler(e)}>
-                зарегестрировать
-                </div>
-          </div>
-        </form>
+        <Form
+          onSubmit={registrationHandler} validate={validate}
+          render={({ handleSubmit, form, submitting }) => (
+            <form onSubmit={handleSubmit}>
+              {
+                inputs.map((el, i) => {
+                  return (
+                    <div className={styles.line} key={i}>
+                      <Field name={el.name}>
+                        {({ input, meta }) => (
+                          <div>
+                            <input {...input} type={'text'} placeholder={el.text} />
+                            {meta.error && meta.touched && <div className={styles.error}>{meta.error}</div>}
+                          </div>
+                        )}
+                      </Field>
+                    </div>
+                  )
+                })
+              }
+              <div className={classnames(styles.line, styles.button)}>
+                <button type='submit' disabled={submitting}>зарегистрировать</button>
+                <button onClick={form.reset}>Очистить</button>
+              </div>
+            </form>
+          )}
+        />
       </div>
     </div>
   )
